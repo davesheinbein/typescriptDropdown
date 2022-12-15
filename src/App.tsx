a26467d12c6505a0a5dfb60221b3bd43c6da323c
David Sheinbein <davesheinbein@gmail.com>
2022-12-14 22:49:01 -0800

    update select

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,17 @@ import './App.css'
 function App() {
   // const [count, setCount] = useState(0)
 
+  const options = [
+    {label: "first", value: 1},
+    {label: "two", value: 2},
+    {label: "three", value: 3},
+    {label: "four", value: 4},
+    {label: "five", value: 5},
+  ]
+
   return (
     <div className="app">
-      <Select />
+      <Select options={options} />
     </div>
   )
 }
